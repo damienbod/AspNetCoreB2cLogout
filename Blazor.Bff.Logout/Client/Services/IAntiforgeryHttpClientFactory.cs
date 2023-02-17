4ca87113ad72f42496643ad6ae4dc6c448c29a8d
damienbod <damien_bod@hotmail.com>
2023-02-17 20:41:17 +0100

    Update BFF

---
+++ Blazor.Bff.Logout/Client/Services/IAntiforgeryHttpClientFactory.cs
@@ -0,0 +1,6 @@
+﻿namespace Blazor.Bff.Logout.Client.Services;
+
+public interface IAntiforgeryHttpClientFactory
+{
+    Task<HttpClient> CreateClientAsync(string clientName = "authorizedClient");
+}
