067179ab0eed1bcc363beda539e80de920bf9a13
damienbod <damien_bod@hotmail.com>
2023-02-17 22:47:03 +0100

    test

---
+++ Blazor.Bff.Logout/Server/CookieEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+namespace Blazor.Bff.Logout.Server;
+
+public class CookieEventHandler : CookieAuthenticationEvents
+{
+    private readonly ILogger<CookieEventHandler> _logger;
+
+    public CookieEventHandler(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<CookieEventHandler>();
+    }
+
+    public override async Task ValidatePrincipal(CookieValidatePrincipalContext context)
+    {
+        if (context.Principal!.Identity!.IsAuthenticated)
+        {
+            _logger.LogInformation("BC ValidatePrincipal: {IsAuthenticated}", 
+                context.Principal.Identity.IsAuthenticated);
+
+            if (false)
+            {
+                context.RejectPrincipal();
+                await context.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                await context.HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+            }
+        }
+    }
+}
--- Blazor.Bff.Logout/Server/Program.cs
+++ Blazor.Bff.Logout/Server/Program.cs
@@ -2,6 +2,7 @@
 using Blazor.Bff.Logout.Server.Services;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 
@@ -30,20 +31,52 @@ services.AddAntiforgery(options =>
 services.AddHttpClient();
 services.AddOptions();
 
-services.AddMicrosoftIdentityWebAppAuthentication(configuration, "AzureB2C")
-    .EnableTokenAcquisitionToCallDownstreamApi(Array.Empty<string>())
-    .AddInMemoryTokenCaches();
+//services.AddMicrosoftIdentityWebAppAuthentication(configuration, "AzureB2C")
+//    .EnableTokenAcquisitionToCallDownstreamApi(Array.Empty<string>())
+//    .AddInMemoryTokenCaches();
 
-services.Configure<MicrosoftIdentityOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
-{
-    options.Events.OnTokenValidated = async context =>
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(authOptions =>
     {
-        if (applicationServices != null && context.Principal != null)
+        configuration.Bind("AzureB2C", authOptions);
+    }
+    , sessionOptions =>
+    {
+        // This appears to be obsolete. Don't use it because you get the following error | OptionsValidationException: Cookie.Expiration is ignored, use ExpireTimeSpan instead.
+        //sessionOptions.Cookie.Expiration = TimeSpan.FromMinutes(-1);
+
+        // Setting the MaxAge will convert Expiration Date on the cookie from 'Session' to an actual future date and will show the default expiration date that is 14 days in the
+        // future. However, note that the session cookie is different from the Authentication cookie, who's MaxAge is set in the AddMicrosoftIdentityWebApp extension method above 
+        // with an expiration that is equal to the session cookie's expiration. Additionally, we set the SlidingExpiration to false. These three configurations are considered the
+        // safest and most secure. Otherwise, not setting the MaxAge while leaving the SlidingExpiration false results in CORS policy errors on the console after the session
+        // cookie has expired. 
+        // Reference: https://brokul.dev/authentication-cookie-lifetime-and-sliding-expiration
+        sessionOptions.Cookie.MaxAge = TimeSpan.FromMinutes(5);
+        sessionOptions.Cookie.Name = "mycookie.Auth";
+        sessionOptions.ExpireTimeSpan = TimeSpan.FromHours(2);
+        sessionOptions.Events.OnValidatePrincipal = async e =>
         {
-            var test = "";
-        }
-    };
-});
+            var claims = e.HttpContext.User.Claims.ToList();
+
+        };
+
+        // Keep this false and never change it to true. The problem with SlidingExpiration enabled is that the authentication cookie could be potentially re-issued infinitely.
+        // That's not a good security practice. If a hacker took control of the account, they could use it forever.
+        sessionOptions.SlidingExpiration = false;
+    }).EnableTokenAcquisitionToCallDownstreamApi(Array.Empty<string>())
+      .AddInMemoryTokenCaches();;
+
+//services.Configure<MicrosoftIdentityOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
+//{
+//    options.Events.OnTokenValidated = async context =>
+//    {
+//        if (applicationServices != null && context.Principal != null)
+//        {
+//            var test = "";
+//        }
+//    };
+
+//});
 
 services.AddControllersWithViews(options =>
     options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
